2ff94d18dd1c7963e5ab10c9c2e743489436a5cf
3s <130720364+3s-Lucius@users.noreply.github.com>
2025-12-29 16:46:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+import requests
 import os
 
 app = Flask(__name__)
 
 VERIFY_TOKEN = "abc"
+PAGE_ACCESS_TOKEN = "EAAMpHdEsTpoBQYSeZCLhz0fQ8z7YQEA5sdswienq5TtV6tdhlZCG1bW7RJyjZBCQypunWNDSc20aMfJe8l5kfHw1dc7FZBcEsHTbNM8NkAf4JRqNUx4NMpPPpg75U2ZA5t9LRDPX8cBpldKjnB1ZAyUkUtvBRnjTnWK96NYY8TdEQptPNAdV2zQVXlffCNZCNlsy3SvjGV7XHYRqGwYPhNyEytgKXw0osflHnjVLwZDZD"
 
 @app.route("/")
 def home():
@@ -14,12 +16,28 @@ def webhook():
     if request.method == "GET":
         token = request.args.get("hub.verify_token")
         challenge = request.args.get("hub.challenge")
-
         if token == VERIFY_TOKEN:
             return challenge
         return "Sai token", 403
 
+    data = request.get_json()
+    for entry in data.get("entry", []):
+        for event in entry.get("messaging", []):
+            if "message" in event:
+                sender_id = event["sender"]["id"]
+                text = event["message"].get("text")
+                if text:
+                    send_message(sender_id, f"Bạn vừa nói: {text}")
     return "ok", 200
+
+def send_message(psid, text):
+    url = "https://graph.facebook.com/v18.0/me/messages"
+    params = {"access_token": PAGE_ACCESS_TOKEN}
+    payload = {
+        "recipient": {"id": psid},
+        "message": {"text": text}
+    }
+    requests.post(url, params=params, json=payload)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
